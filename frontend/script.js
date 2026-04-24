0201521d99010542871d56fba77fe4a6c03f2cab
SebastianCastellon <sebastiandenniscaste@gmail.com>
2026-04-24 23:03:26 +0200

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@
 // ============================================
 // URL del backend en Render (actualizar después del despliegue)
 // Mientras pruebas local, usa localhost
-const API_URL = 'https://umsa-cloud-feria.onrender.com/';  // ← Cambiar por tu URL de Render
+const API_URL = 'https://umsa-cloud-feria.onrender.com'; // ← Cambiar por tu URL de Render
 
 // ============================================
 // FUNCIÓN PRINCIPAL - BUSCAR ESTUDIANTE
